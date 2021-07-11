4129b64b28aa5c2bc8fed9dd762ef3c36fd76f65
amirsantoso <amir.santosos@gmail.com>
2021-07-11 12:12:34 +0700

    Update index.php
    update Commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['email'])) {
 // ----------------------------------CONTENT HERE---------------------------------- //
-    echo '<center><h1>Selamat Datang ;)</h1><br/><a href="./logout.php">Logout</a>';
+    echo '<center><h1>Selamat Datang di Halaman Website Pertama Heroku</h1><br/><a href="./logout.php">Logout</a>';
 // ----------------------------------CONTENT HERE---------------------------------- //
 
 } else {
